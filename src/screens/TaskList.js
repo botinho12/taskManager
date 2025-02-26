2b0276d3dfd7cee8a67ecc33b2f70f7589f12914
Botinho12 <Botivinicios77@gmail.com>
2025-02-26 19:28:03 -0300

    feat: adicionando estilos ao task manager

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,8 +1,49 @@
-import { View , Text} from "react-native"
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
+
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default function TaskList() {
-    return 
-        <View>
-            <Text>TaskList</Text>
+    return(
+        <View style={styles.container}>
+            <ImageBackground source={todayImage} style={styles.background}>
+
+                <View style={styles.titleBar}>
+                    <Text style={styles.title}>Hoje</Text>
+                    <Text style={styles.subtitle}>24 de Fevereiro</Text>
+                </View>
+
+            </ImageBackground>
+            <View style={styles.taskList}>
+                <Text>Task #01</Text>
+            </View>
+
         </View>
+    )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    }, 
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title: {
+        color: 'white',
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subtitle: {
+        color: 'white',
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
+    }
+})
