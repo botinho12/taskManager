776fd7642589aa0e7ee5d8f3067f541085cfc003
Botinho12 <botivinicios77@gmail.com>
2025-05-14 20:30:40 -0300

    Inicia o desenvolvimento da funcionalidade de exclusão de tarefa

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,27 @@
-import { View, Text, StyleSheet, TouchableWithoutFeedback } from "react-native"
+import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity } from "react-native"
 
 import Icon from 'react-native-vector-icons/FontAwesome'
+
+import { Swipeable } from "react-native-gesture-handler"
 
 import moment from "moment-timezone"
 import 'moment/locale/pt-br'
 
 export default props => {
 
-    const doneOrNotStyle = props.doneAt ? {textDecorationLine: 'line-through'} : {}
+    const doneOrNotStyle = props.doneAt ? { textDecorationLine: 'line-through' } : {}
 
     const date = props.doneAt ? props.doneAt : props.estimateAt
     const formattedDate = moment(date).tz('America/Sao_Paulo').locale('pt-br').format('ddd, D [de] MMMM')
+
+    const getRightContent = () => {
+        return (
+            <TouchableOpacity style={styles.right}
+                onPress={() => props.onDelete && props.onDelete(props.id)}>
+                <Icon name="trash" size={30} color="#fff" />
+            </TouchableOpacity>
+        )
+    }
 
     return (
         <View style={styles.container}>
@@ -28,14 +39,14 @@ export default props => {
 }
 
 function getCheckView(doneAt) {
-    if(doneAt != null) {
-        return(
+    if (doneAt != null) {
+        return (
             <View style={styles.done}>
-                <Icon name='check' size={20} color='#fff'/>
+                <Icon name='check' size={20} color='#fff' />
             </View>
         )
     } else {
-        return(
+        return (
             <View style={styles.pending}></View>
         )
     }
@@ -62,7 +73,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#555'
     },
-    done : {
+    done: {
         height: 25,
         width: 25,
         borderRadius: 13,
